d6fa7adb806c7a15fa59dec6277f41a0096aa3bb
Harshithalekkala <80406614+Harshithalekkala@users.noreply.github.com>
2022-05-27 16:44:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+img="";
+
+function preload() {
+   img=loadImage ('dog_cat.jpg');
+}
+
+function setup() {
+    canvas=createCanvas(640,420);
+    canvas.center();
+
+}
+
+function draw() {
+  image(img,0,0,640,420);
+  fill('goldenrod');  
+  text("dog",45,75);
+  noFill();
+  stroke('goldenrod')
+  rect(30,60,450,350);
+}
